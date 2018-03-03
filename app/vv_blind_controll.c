c05797eb0f43a55364f7e446194fbbdd397dd763
Vladislav Vavra <vlvavra@cisco.com>
2018-03-03 21:34:55 +0100

    Controll blind relay

--- app/vv_blind_controll.c
+++ app/vv_blind_controll.c
@@ -1,5 +1,6 @@
 
 #include <bc_module_power.h>
+#include <bc_gpio.h>
 
 #include "vv_blind_controll.h"
 
@@ -7,26 +8,30 @@ void _vv_blind_controller_task();
 
 void vv_blind_init() {
     vv_blind_controller.task = bc_scheduler_register(_vv_blind_controller_task, NULL, BC_TICK_INFINITY);
+    bc_gpio_init(BC_GPIO_P14);
+    bc_gpio_set_mode(BC_GPIO_P14, BC_GPIO_MODE_OUTPUT);
 }
 
 void vv_blind_move() {
     switch(vv_blind_controller.state) {
 	case GOING_UP:
 	    bc_module_power_relay_set_state(true);
+        bc_gpio_set_output(BC_GPIO_P14, true);
 	    vv_blind_controller.state = STOPPED_AFTER_GOING_UP;
         bc_scheduler_plan_relative(vv_blind_controller.task, 10000);
         break;
 	case STOPPED_AFTER_GOING_UP: 
-	    // Not yet implemented
+        bc_gpio_set_output(BC_GPIO_P14, false);
 	    vv_blind_controller.state = GOING_DOWN;
 	    break;
-	case GOING_DOWN: 
+	case GOING_DOWN:
+        bc_gpio_set_output(BC_GPIO_P14, true);
 	    bc_module_power_relay_set_state(false);
 	    vv_blind_controller.state = STOPPED_AFTER_GOING_DOWN;
         bc_scheduler_plan_relative(vv_blind_controller.task, 10000);
 	    break;
 	case STOPPED_AFTER_GOING_DOWN: 
-	    // Not yet implemented
+        bc_gpio_set_output(BC_GPIO_P14, false);
 	    vv_blind_controller.state = GOING_UP;
 	    break;
 	default: break;
@@ -45,7 +50,11 @@ void vv_blind_go_down() {
 void _vv_blind_controller_task() {
     switch(vv_blind_controller.state) {
         case GOING_UP:
+            vv_blind_controller.state = STOPPED_AFTER_GOING_UP;
+            vv_blind_move();
+            break;
         case GOING_DOWN:
+            vv_blind_controller.state = STOPPED_AFTER_GOING_DOWN;
             vv_blind_move();
             break;
         default: break;
