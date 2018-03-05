1499f4ca068daf1a5d2f531d33816281496b0ac0
Vladislav Vavra <vlvavra@cisco.com>
2018-03-05 07:21:46 +0100

    Refactor blind state to named enum

--- app/vv_blind_controll.h
+++ app/vv_blind_controll.h
@@ -3,16 +3,19 @@
 
 static const bc_gpio_channel_t BLIND_POWER_RELAY_GPIO = BC_GPIO_P8;
 static const int BLIND_STOP_TIMEOUT = 30000;
-struct {
-    bc_scheduler_task_id_t task;
-    enum {
-        GOING_UP,
-        STOPPED_AFTER_GOING_UP,
-        GOING_DOWN,
-        STOPPED_AFTER_GOING_DOWN,
-    } state;
 
+enum blind_state_t {
+    GOING_UP,
+    STOPPED_AFTER_GOING_UP,
+    GOING_DOWN,
+    STOPPED_AFTER_GOING_DOWN,
+};
+
+struct vv_blind_controller_t {
+    bc_scheduler_task_id_t task;
+    enum blind_state_t state;
 } vv_blind_controller;
+
 void vv_blind_init();
 void vv_blind_move();
 void vv_blind_go_up();
